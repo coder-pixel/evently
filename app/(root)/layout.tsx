45523db3e8189081c9414cd97a92b37b83de0f26
sauvik <sauvik@logic-square.com>
2025-01-14 16:23:16 +0530

    clerk setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "A simple management app to create and manage events.",
+  icons: {
+    icon: "./favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -11,6 +12,9 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Evently",
   description: "A simple management app to create and manage events.",
+  icons: {
+    icon: "./favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -19,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins?.variable} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins?.variable} antialiased`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
