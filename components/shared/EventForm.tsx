0276f13145908087b07c81d66e87b89072baac6c
sauvik <sauvik@logic-square.com>
2025-01-17 20:44:24 +0530

    database connection + upload things file uploader started

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,128 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "../ui/input";
+import { eventFormSchema } from "@/lib/validator";
+import * as z from "zod";
+import { eventDefaultValues } from "@/constants";
+import Dropdown from "./Dropdown";
+import { Textarea } from "../ui/textarea";
+import FileUploader from "./FileUploader";
+
+type EventFormProps = {
+  userId: string;
+  type: "Create" | "Update";
+};
+
+const initialValues = eventDefaultValues;
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-5"
+      >
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Event Title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Dropdown
+                    value={field?.value}
+                    onChangeHandler={field?.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="h-72">
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    className="textarea rounded-2xl"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="h-72">
+                  <FileUploader
+                    onFieldChange={field?.onChange}
+                    imageUrl={field?.value}
+                    setFiles={setFiles}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default EventForm;
---
+++ components/shared/FileUploader.tsx
@@ -0,0 +1,17 @@
+import React, { Dispatch, SetStateAction } from "react";
+
+type FileUploaderProps = {
+  onFieldChange: (url: string) => void;
+  imageUrl: string;
+  setFiles: Dispatch<SetStateAction<File[]>>;
+};
+
+const FileUploader = ({
+  imageUrl,
+  onFieldChange,
+  setFiles,
+}: FileUploaderProps) => {
+  return <div>FileUploader</div>;
+};
+
+export default FileUploader;
